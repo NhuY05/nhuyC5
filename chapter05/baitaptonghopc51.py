9a929473fe0c5cc8814b4f0468f10d10f6a4df87
NhuY05 <nguyenthinhuy.iuh@gmail.com>
2024-09-16 15:38:10 +0700

    Update baitaptonghopc51.py

--- chapter05/baitaptonghopc51.py
+++ chapter05/baitaptonghopc51.py
@@ -4,35 +4,32 @@ Created on Sat Sep 14 02:16:13 2024
 
 @author: NGUYEN THI NHU Y
 """
+chuoi = input("Nhập một chuỗi: ")
 
-str_input = input("Nhập chuỗi: ")
-dodaichuoi = len(str_input)
-print("Độ dài chuỗi: ", dodaichuoi)
-kitudacbiet = 0
-kituthuong = 0
-kituso = 0
-kituhoa = 0
+length = len(chuoi)
+print("Độ dài chuỗi: ", length)
 
-daykitudacbiet = []
-daykituthuong = []
-daykituso = []
-daykituhoa = []
-cackitudacbiet = set("!@#$%^&*()-=+/")
+kydb = "!@#$%^&*()-=+./"
+dem_kydb = 0
+for i in chuoi:
+    if i in kydb:
+        dem_kydb += 1
+print("Số ký tự đặc biệt: ", dem_kydb)
 
-for kitu in str_input:
-    if kitu in cackitudacbiet:
-        kitudacbiet += 1
-        daykitudacbiet.append(kitu)
-    elif kitu.islower():
-        kituthuong += 1
-        daykituthuong.append(kitu)
-    elif kitu.isdigit():
-        kituso += 1
-        daykituso.append(kitu)
-    elif kitu.isupper():
-        kituhoa += 1
-        daykituhoa.append(kitu)
-print("Số kí tự đặt biệt: ", kitudacbiet, "Các ký tự đó là: ", ",".join(daykitudacbiet))
-print("Số kí tự [a-z]: ", kituthuong, "Các ký tự đó là: ", ",".join(daykituthuong))
-print("Số kí tự [0-9]: ", kituso, "Các ký tự đó là: ", ",".join(daykituso))
-print("Số kí tự [A-Z]: ", kituhoa, "Các ký tự đó là: ", ",".join(daykituhoa))
+dem_thuong = 0
+for i in chuoi:
+    if i.islower():
+        dem_thuong += 1
+print("Số ký tự chữ cái thường [a-z]: ", dem_thuong)
+
+dem_so = 0
+for i in chuoi:
+    if i.isdigit():
+        dem_so += 1
+print("Số ký tự chữ số [0-9]: ", dem_so)
+
+dem_hoa = 0
+for i in chuoi:
+    if i.isupper():
+        dem_hoa += 1
+print("Số ký tự chữ cái hoa [A-Z]: ", dem_hoa)
